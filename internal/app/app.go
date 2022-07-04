069a19237f150ce438ce292270baf97d3e256843
Roman Nikulenkov <nrw@yandex.ru>
2022-07-04 12:59:15 +0300

    fix json

--- internal/app/app.go
+++ internal/app/app.go
@@ -14,7 +14,7 @@ import (
 	"github.com/n-r-w/postgres"
 )
 
-const version = "1.0.3"
+const version = "1.0.4"
 
 func Start(cfg *config.Config, logger lg.Logger) {
 	logger.Info("logsrv %s", version)
